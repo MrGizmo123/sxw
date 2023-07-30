a2351f7d0f570ce12b00e86e9cc4a08d1da469f2
liquidcat <hrushikeshp13@gmail.com>
2023-07-30 20:18:18 +0530

    update README

--- src/config.h
+++ src/config.h
@@ -1,21 +1,21 @@
 enum { SchemeRed, SchemeGreen, SchemeYellow, SchemeBlue, SchemePurple, SchemeAqua, SchemeLightGrey, SchemeLast };
 
-/* static const char* colors[SchemeLast][2] = { */
-/*         [SchemeRed]        = { "#fb4f33", "#222222"}, */
-/*         [SchemeGreen]      = { "#b8bb26", "#222222"}, */
-/*         [SchemeYellow]     = { "#fabd2f", "#222222"}, */
-/*         [SchemeBlue]       = { "#83a598", "#222222"}, */
-/*         [SchemePurple]     = { "#d3869b", "#222222"}, */
-/*         [SchemeAqua]       = { "#8ec07c", "#222222"}, */
-/* 	   [SchemeLightGrey]  = { "#ffffff", "#444444"}, */
-/* }; */
-
 static const char* colors[SchemeLast][2] = {
-        [SchemeRed]        = { "#bb0f03", "#ffffb0"},
-        [SchemeGreen]      = { "#989b06", "#ffffb0"},
-        [SchemeYellow]     = { "#da9d0f", "#ffffb0"},
-        [SchemeBlue]       = { "#6385a8", "#ffffb0"},
-        [SchemePurple]     = { "#b3567b", "#ffffb0"},
-        [SchemeAqua]       = { "#6ea05c", "#ffffb0"},
-	[SchemeLightGrey]  = { "#ffffff", "#ffffb0"}
+        [SchemeRed]        = { "#fb4f33", "#222222"},
+        [SchemeGreen]      = { "#b8bb26", "#222222"},
+        [SchemeYellow]     = { "#fabd2f", "#222222"},
+        [SchemeBlue]       = { "#83a598", "#222222"},
+        [SchemePurple]     = { "#d3869b", "#222222"},
+        [SchemeAqua]       = { "#8ec07c", "#222222"},
+        [SchemeLightGrey]  = { "#ffffff", "#444444"},
 };
+
+/* static const char* colors[SchemeLast][2] = { */
+/*         [SchemeRed]        = { "#bb0f03", "#ffffb0"}, */
+/*         [SchemeGreen]      = { "#989b06", "#ffffb0"}, */
+/*         [SchemeYellow]     = { "#da9d0f", "#ffffb0"}, */
+/*         [SchemeBlue]       = { "#6385a8", "#ffffb0"}, */
+/*         [SchemePurple]     = { "#b3567b", "#ffffb0"}, */
+/*         [SchemeAqua]       = { "#6ea05c", "#ffffb0"}, */
+/* 	[SchemeLightGrey]  = { "#ffffff", "#ffffb0"} */
+/* }; */
